16fde56d4fd3a330e86dd70e2e30a5c90ee3f8a7
Luciano Valente <lucha.valente@gmail.com>
2022-05-28 20:01:11 +0200

    Initial Push

---
+++ mysharedcode/lib.py
@@ -0,0 +1,11 @@
+
+
+def try_me(sed = 'poca'):
+    """"
+    funcion de prueba para compartir en el bootcamp
+    """
+
+    if sed == 'poca':
+        return 'si tenes poca sed, tomate solo una cerveza'
+
+    return 'si tenes mucha sed, tomate mas de una'
